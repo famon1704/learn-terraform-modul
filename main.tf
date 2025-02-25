a924333f1eadcfd67069d62a355640a0478be49a
nama <fairuzu@Kiritos-MacBook-Pro.local>
2025-02-25 15:25:48 +0700

    Update AMI to Debian

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ module "ec2_instance" {
   count = 2
   name = "my-ec2-cluster-${count.index}"
 
-  ami                   = "ami-0c5204531f799e0c6"
+  ami                   = "ami-03e383d33727f4804"
   instance_type         = "t2.micro"
   vpc_security_group_ids = [module.vpc.default_security_group_id]
   subnet_id             = module.vpc.public_subnets[0]
